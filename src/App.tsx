b168fe9ee8f098dfbd41b4a8da288296ec6a1632
wslfung <wilson.sl.fung@gmail.com>
2024-10-13 11:59:38 -0700

    updated libraries

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
+import { PrimeReactProvider } from 'primereact/api'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import '/node_modules/primeflex/primeflex.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <PrimeReactProvider>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,7 +30,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </PrimeReactProvider>
   )
 }
 
