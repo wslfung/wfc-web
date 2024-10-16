1a1cb38a942e3771bb52587c2fad42628fe46584
wslfung <wilson.sl.fung@gmail.com>
2024-10-16 16:43:07 -0700

    additional config for mdx

--- src/routes/markdownCreate/MarkdownCreate.tsx
+++ src/routes/markdownCreate/MarkdownCreate.tsx
@@ -1,4 +1,4 @@
-import { BlockTypeSelect, BoldItalicUnderlineToggles, CodeToggle, diffSourcePlugin, DiffSourceToggleWrapper, headingsPlugin, imagePlugin, InsertImage, InsertTable, InsertThematicBreak, linkDialogPlugin, linkPlugin, listsPlugin, ListsToggle, markdownShortcutPlugin, MDXEditor, MDXEditorMethods, quotePlugin, tablePlugin, thematicBreakPlugin, toolbarPlugin, UndoRedo } from '@mdxeditor/editor'
+import { BlockTypeSelect, BoldItalicUnderlineToggles, codeBlockPlugin, CodeToggle, CreateLink, diffSourcePlugin, DiffSourceToggleWrapper, headingsPlugin, imagePlugin, InsertCodeBlock, InsertImage, InsertTable, InsertThematicBreak, linkDialogPlugin, linkPlugin, listsPlugin, ListsToggle, markdownShortcutPlugin, MDXEditor, MDXEditorMethods, quotePlugin, StrikeThroughSupSubToggles, tablePlugin, thematicBreakPlugin, toolbarPlugin, UndoRedo } from '@mdxeditor/editor'
 import '@mdxeditor/editor/style.css'
 import { useRef } from 'react'
 
@@ -7,7 +7,7 @@ export default function MarkdownCreate() {
   const mdxEditorRef = useRef<MDXEditorMethods>(null)
 
   return (
-    <MDXEditor ref={mdxEditorRef} className="p-component" markdown='# Hello World' plugins={[headingsPlugin(), listsPlugin(), quotePlugin(), thematicBreakPlugin(), imagePlugin(), tablePlugin(), linkDialogPlugin(), linkPlugin(), markdownShortcutPlugin(),
+    <MDXEditor ref={mdxEditorRef} className="p-component" markdown='# Hello World' plugins={[headingsPlugin(), listsPlugin(), quotePlugin(), thematicBreakPlugin(), imagePlugin(), tablePlugin(), linkDialogPlugin(), linkPlugin(), markdownShortcutPlugin(), codeBlockPlugin(), 
     diffSourcePlugin({
       viewMode: 'rich-text'
     }),
@@ -18,11 +18,14 @@ export default function MarkdownCreate() {
           <DiffSourceToggleWrapper>
             <UndoRedo />
             <BoldItalicUnderlineToggles />
+            <StrikeThroughSupSubToggles />
             <BlockTypeSelect />
             <ListsToggle />
+            <CreateLink />
             <InsertImage />
             <InsertTable />
             <InsertThematicBreak />
+            {/* <InsertCodeBlock /> */}
             <CodeToggle />
           </DiffSourceToggleWrapper>
         </>
