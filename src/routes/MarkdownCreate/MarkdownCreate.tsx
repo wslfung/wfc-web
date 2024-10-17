90b77f757383ef78580bd1ce639ee724a1ddf603
wslfung <wilson.sl.fung@gmail.com>
2024-10-17 13:42:34 -0700

    adjusted some markdown rendering to work better with primereact

--- src/routes/MarkdownCreate/MarkdownCreate.tsx
+++ src/routes/MarkdownCreate/MarkdownCreate.tsx
@@ -1,20 +1,31 @@
-import { BlockTypeSelect, BoldItalicUnderlineToggles, codeBlockPlugin, CodeToggle, CreateLink, diffSourcePlugin, DiffSourceToggleWrapper, headingsPlugin, imagePlugin, InsertCodeBlock, InsertImage, InsertTable, InsertThematicBreak, linkDialogPlugin, linkPlugin, listsPlugin, ListsToggle, markdownShortcutPlugin, MDXEditor, MDXEditorMethods, quotePlugin, StrikeThroughSupSubToggles, tablePlugin, thematicBreakPlugin, toolbarPlugin, UndoRedo } from '@mdxeditor/editor'
+import { BlockTypeSelect, BoldItalicUnderlineToggles, CodeToggle, CreateLink, diffSourcePlugin, DiffSourceToggleWrapper, headingsPlugin, imagePlugin, InsertImage, InsertTable, InsertThematicBreak, linkDialogPlugin, linkPlugin, listsPlugin, ListsToggle, markdownShortcutPlugin, MDXEditor, MDXEditorMethods, quotePlugin, StrikeThroughSupSubToggles, tablePlugin, thematicBreakPlugin, toolbarPlugin, UndoRedo } from '@mdxeditor/editor'
 import '@mdxeditor/editor/style.css'
-import { Button } from 'primereact/button';
-import { useRef, useState } from 'react'
-import Markdown from 'react-markdown';
+import { Button as PRButton } from 'primereact/button';
+import { Checkbox } from 'primereact/checkbox';
+import React, { useRef, useState } from 'react'
+import Markdown, { ExtraProps } from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import rehypeRaw from 'rehype-raw';
+import './MarkdownCreate.css'
 
 export default function MarkdownCreate() {
 
   const mdxEditorRef = useRef<MDXEditorMethods>(null)
 
-  const [ markdown, setMarkdown ] = useState<string>('# Hello World')
+  const [ markdown, setMarkdown ] = useState<string>(`# Hello World
+
+[google](https://www.google.com "google")
+
+
+
+
+
+* [x] dfsdfsdf
+* [ ] dfsdf`)
 
   return (
     <div className='flex flex-column min-h-screen'>
-      <MDXEditor ref={mdxEditorRef} className="flex-grow-1" markdown={markdown} plugins={[headingsPlugin(), listsPlugin(), quotePlugin(), thematicBreakPlugin(), imagePlugin(), tablePlugin(), linkDialogPlugin(), linkPlugin(), markdownShortcutPlugin(), codeBlockPlugin(), 
+      <MDXEditor ref={mdxEditorRef} className="flex-grow-1" markdown={markdown} plugins={[headingsPlugin(), listsPlugin(), quotePlugin(), thematicBreakPlugin(), imagePlugin(), tablePlugin(), linkDialogPlugin(), linkPlugin(), markdownShortcutPlugin(), 
       diffSourcePlugin({
         viewMode: 'rich-text'
       }),
@@ -32,13 +43,12 @@ export default function MarkdownCreate() {
               <InsertImage />
               <InsertTable />
               <InsertThematicBreak />
-              {/* <InsertCodeBlock /> */}
               <CodeToggle />
             </DiffSourceToggleWrapper>
           </>
         )
-      })]} />
-      <Button className='flex-grow-0' label='Convert' onClick={()=>{
+      })]} contentEditableClassName='p-component wf-markdown' />
+      <PRButton link className='flex-grow-0' label='Convert' onClick={()=>{
         if (mdxEditorRef.current) {
           setMarkdown(mdxEditorRef.current.getMarkdown())
           console.log("state is set")
@@ -46,7 +56,26 @@ export default function MarkdownCreate() {
           console.log("mdxeditorref is null")
         }
       }} />
-      <Markdown className="p-component" remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw]}>{markdown}</Markdown>
+      <Markdown className='p-component wf-markdown' remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw]} components={{
+        a: (props: JSX.IntrinsicElements["a"] & ExtraProps) => {
+          const {children, node, ...rest} = props
+          if (React.isValidElement(children)) {
+            return (
+              <PRButton link onClick={()=> window.open(rest['href'], '_blank')}>{children}</PRButton>
+            )  
+          } else {
+            return (
+              <PRButton link label={children?children.toString():''} onClick={()=> window.open(rest['href'], '_blank')} />
+            )  
+          }
+        },
+        input: (props: JSX.IntrinsicElements["input"] & ExtraProps) => {
+          const {children, node, ...rest} = props
+          return (
+            <Checkbox value={children} checked={rest["checked"]?rest["checked"]:false} disabled />
+          )
+        }
+      }}>{markdown}</Markdown>
     </div>
   )
 }
