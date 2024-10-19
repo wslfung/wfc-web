ae0e723f93659da07b4cc91e9a440ccb5afbf2ba
wslfung <wilson.sl.fung@gmail.com>
2024-10-18 18:28:06 -0700

    adjusted button style and sizing in footer

--- src/Home.tsx
+++ src/Home.tsx
@@ -8,7 +8,7 @@ function Home() {
 
   return (
     <WFStandardPageTemplate currentPage="Home" pageTitle="Home - wilsonfung.ca">
-        <Card className="m-0 md:m-4 border-noround md:border-round-md overflow-auto flex-grow-1 max-h-full">
+        <Card className="m-0 md:m-4 mb-2 border-noround md:border-round-md overflow-auto flex-grow-1 max-h-full">
           <p>hello</p><p>hello</p><p>hello</p><p>hello</p><p>hello</p><p>hello</p><p>hello</p><p>hello</p><p>hello</p><p>hello</p><p>hello</p><p>hello</p><p>hello</p><p>hello</p><p>hello</p><p>hello</p><p>hello</p><p>hello</p><p>hello</p><p>hello</p><p>hello</p><p>hello</p><p>hello</p><p>hello</p><p>hello</p><p>hello</p><p>hello</p><p>hello</p><p>hello</p><p>hello</p><p>hello</p><p>hello</p><p>hello</p><p>hello</p><p>hello</p><p>hello</p><p>hello</p><p>hello</p><p>hello</p><p>hello</p><p>hello</p><p>hello</p><p>hello</p><p>hello</p><p>hello</p><p>hello</p>
         </Card>
     </WFStandardPageTemplate>
--- src/components/Footer/WFStandardPageFooter.tsx
+++ src/components/Footer/WFStandardPageFooter.tsx
@@ -9,10 +9,10 @@ export default function WFStandardPageFooter() {
   const navigate = useNavigate()
 
   return (
-    <div className='flex flex-row justify-content-end min-w-full pt-0 pb-2 pr-3 md:pr-6 surface-700'>
+    <div className='flex flex-row justify-content-end min-w-full pt-0 pb-2 pr-2 md:pr-6 surface-700'>
       <span className=''>
-        <Button link onClick={()=>{navigate('/me/resume')}}><span className='wf-footer-icon pi pi-book text-4xl p-2 pt-0 text-color-secondary' /><span className='wf-footer-text'>Resume</span></Button>
-        <Button link onClick={()=>{window.open('https://www.linkedin.com/in/wilson-sl-fung/', '_blank')}}><span className='wf-footer-icon pi pi-linkedin text-4xl p-2 pt-0 text-color-secondary' /><span className='wf-footer-text'>Linkedin</span></Button>
+        <Button className='text-sm lg:text-base' text icon='pi pi-book text-lg lg:text-2xl' label='Resume' onClick={()=>{navigate('/me/resume')}} />
+        <Button className='text-sm lg:text-base' text icon='pi pi-linkedin text-lg lg:text-2xl' label='Linkedin' onClick={()=>{window.open('https://www.linkedin.com/in/wilson-sl-fung/', '_blank')}} />
       </span>
     </div>
   )
